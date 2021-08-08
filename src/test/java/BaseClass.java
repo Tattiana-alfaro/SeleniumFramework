851a5397b8ae4fe1e4189802d603f40311c90240
Tattiana-alfaro <t$77Sto30yru>
2021-08-08 12:02:29 -0600

    Add TestSearch

---
+++ src/test/java/BaseClass.java
@@ -0,0 +1,71 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest(){
+        //System.out.println("*BeforeTest");
+    }
+
+    @Parameters({"browser"})
+    @BeforeMethod
+    public void beforeMethod(@Optional("chrome") String browser){
+
+        switch (browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "IE":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+
+        }
+
+        String URL = "https://demo.opencart.com/";
+
+        driver.manage().window().maximize();
+        driver.get(URL);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //
+        //driver.manage().timeouts().setScriptTimeout(15, TimeUnit.SECONDS);//Scripts en correr
+        //driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);//page Load
+    }
+    @AfterTest
+    public void AfterTest(){
+        //System.out.println("*AfterTest");
+    }
+
+    @AfterMethod
+    public void AfterMethod(){
+        //System.out.println("*AfterMethod");
+        TakeScreenshot();
+        driver.close();
+        try {
+            driver.quit();
+        }catch (WebDriverException exception){
+            System.out.println("Error quit browser:" + exception);
+        }
+    }
+
+    @Attachment(value = "screenshot", type = "image/png")
+    public byte[] TakeScreenshot(){
+        return ((TakesScreenshot) this.driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
--- src/test/java/TestAccount.java
+++ src/test/java/TestAccount.java
@@ -1,29 +1,24 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestAccount {
+public class TestAccount extends BaseClass{
 
-    @Test
+
+
+
+    @Description("Validate test loging was successful")
+    @Test(description = "Test Login successful")
     public void  Test_Login_Successful(){
 
         String username = "tattiana.alfarocastaneda@ucreativa.com";
         String password = "Testing2@";
-
-        String URL = "https://demo.opencart.com/";
-        String pathToDriver = Test.class.getResource("/chromedriver.exe").getPath();
-        System.setProperty("webdriver.chrome.driver", pathToDriver);
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //
-        driver.manage().timeouts().setScriptTimeout(15, TimeUnit.SECONDS);//Scripts en correr
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);//page Load
 
         //Go to Login Page
         driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
@@ -36,9 +31,32 @@ public class TestAccount {
 
         WebElement logOutButton = driver.findElement(By.linkText("Logout"));
         Assert.assertTrue(logOutButton.isDisplayed());
-        driver.close();
-        driver.quit();
 
+        }
 
+    @Description("Testing Unsuccessful credentials")
+    @Test(description = "Test Login Unsuccessful")
+    public void  Test_Login_Unsuccessful() {
+
+        String username = "tattiana.alfarocastaneda@ucreativa.com";
+        String password = "Testing3@";
+        String expectedMessage = "warning: no match for e-mail address and/or password.";
+
+        //Go to Login Page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[2]/a")).click();
+
+        //Llenar formulario
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value='Login' and @type ='submit']")).click();
+
+        WebElement warningMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
+        Assert.assertTrue(warningMessage.isDisplayed());
+        Assert.assertEquals(expectedMessage.toLowerCase().trim(), warningMessage.getText().toLowerCase().trim());
     }
+
+
+
+
 }
